cdf54f1e233d0a7014ed21cd7ffae891525cae44
Casey <caseyeaster39@gmail.com>
2021-11-01 19:50:49 -0500

    api key cache added

--- scripts/weather_data.py
+++ scripts/weather_data.py
@@ -1,26 +1,26 @@
 import requests
-import time
+
+
+def get_user_key():
+    try:
+        with open('.key_cache/key.txt', 'r') as fin:
+            key = fin.read()
+    except FileNotFoundError:
+        print('User key not found. You can create an account here: https://home.openweathermap.org/')
+        key = input('Please paste your key here: ')
+        with open('.key_cache/key.txt', 'w') as fout:
+            fout.write(key)
+    return key
 
 
 def weather_req_now(latlong):
     lat = latlong[0]
     lon = latlong[1]
+    key = get_user_key()
 
-    start = time.time()
-    cnt = 1
-    key = '5a37683754d15ce2739e21efd5f84146'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}'
 
-    request_url = 'http://history.openweathermap.org/data/2.5/history/city?'
-
-    payload = {
-        'lat': lat,
-        'lon': lon,
-        'start': start,
-        'cnt': cnt,
-        'key': key
-    }
-
-    resp = requests.get(request_url, params=payload)
+    resp = requests.get(request_url)
     resp.raise_for_status()
 
     return resp.json()
